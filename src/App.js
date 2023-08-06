3a74dc91a0111425e560ba4c0dd43a4703b6fa65
joaoand <joao.demata@correo.unimet.edu.ve>
2023-08-06 11:03:02 -0400

    Css, components y maquetacion de la maquina de estado

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BaseLayout } from './containers/base_layout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/base_layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useMachine } from '@xstate/react';
+import { Nav } from '../components/nav';
+import { StepsLayout } from './steps_layout';
+import bookingMachine from '../machines/booking.machine';
+import './base_layout.css';
+
+export const BaseLayout = () => {
+  const [state, send] = useMachine(bookingMachine);
+
+  console.log('nuestra maquina', state.value);
+
+  return (
+    <div className='BaseLayout'>
+      <Nav state={state} send={send} />
+      <StepsLayout state={state} send={send}/>
+    </div>
+  );
+}
